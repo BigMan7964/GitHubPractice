caecac4986ee0f322d6f521b876c75817e7f06a3
Zuzanna Lasota <zlasota5196@stu.d214.org>
2022-04-04 14:44:53 -0500

    ur annoying

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberTextField : UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenAddButtonPressed ( sender :UIButton){
+        let firstNumberTextField = Double(firstNumberTextField.text!)
+        let secondNumberTextField = Double(secondNumberTextField.text!)
+        let answer = Double(firstNumberTextField! * secondNumberTextField!)
+        answerLabel.text = "The Answer is \(answerLabel)"
+       
+        
+    }
 }
 
